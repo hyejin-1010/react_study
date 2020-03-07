24e06900ac11d033e6faaa32d0a578a245014dd1
Hyejin1010 <hyepago@naver.com>
2020-03-07 23:32:01 +0900

    MOD: react-redux redux 종속성 제거

---
+++ react-redux/src/containers/AddNumber.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import store from '../store';
+import AddNumber from '../components/AddNumber';
+
+export default class extends Component {
+    render() {
+        return <AddNumber onClick={function(size) {
+            store.dispatch({ type: 'INCREMENT', size });
+        }.bind(this)}></AddNumber>
+    }
+}
